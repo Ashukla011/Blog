37a64134ded74c9ff4653166a9bb44e67d0b4f9c
Ashukla011 <shuklavashu9696@gmail.com>
2025-03-17 15:01:04 +0530

    'e-commerce-product-recommendation'

---
+++ e-commerce/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function App() {  
+
+  return (
+    <>
+      <div>
+        <h1>E-Commerce Product Recommendation</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
